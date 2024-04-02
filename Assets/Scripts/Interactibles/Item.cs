5146500202d8dd73ae3bf716372be880edcf6f4c
JoshuaHartop <119062032+JoshuaHartop@users.noreply.github.com>
2024-04-02 14:12:06 -0300

    0.4.10.7
    added model for 2x damage item
    made items spin and bob up and down

--- Assets/Scripts/Interactibles/Item.cs
+++ Assets/Scripts/Interactibles/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Item : MonoBehaviour, IInteractible
 {
     public int slot;
-
+    int upDown = 1; // 1 is up 0 is down
     private ItemSpawner _spawner;
 
     protected virtual void Start()
@@ -13,6 +13,37 @@ public class Item : MonoBehaviour, IInteractible
         _spawner = FindAnyObjectByType<ItemSpawner>();
         Debug.Log(_spawner);
     }
+
+    protected virtual void Update()
+    {
+        bounceItem();
+    }
+
+    protected virtual void bounceItem()
+    {
+        
+        this.transform.Rotate(0,20 * Time.deltaTime,0);
+        if (this.transform.position.y > 1.1f)
+        {
+            upDown = 0;
+        }
+        else if (this.transform.position.y < 1f) // crayfish make this the item height instead of 0.1 so it touches the pad then goes up
+        {
+            upDown = 1;
+        }
+        if (upDown == 1)
+        {
+            transform.position = transform.position + new Vector3(0, 0.02f * Time.deltaTime, 0);
+        }
+        else if (upDown == 0)
+        {
+            transform.position = transform.position + new Vector3(0, -0.02f * Time.deltaTime, 0);
+        }
+        
+    }
+
+
+
 
     public virtual void OnInteract() {}
 
